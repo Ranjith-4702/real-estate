741efd7e194fd4186753314e66cb52d848463c6d
Ranjith-4702 <ranjith.ks3004@gmail.com>
2026-03-05 19:07:11 +0530

    Added pages folder and updated App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { BrowserRouter , Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Signin from './pages/signin'
+import Signout from './pages/signout'
+import Profile from './pages/profile'
 export default function App() {
   return (
-    <div className='bg-red-600'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<Signin />} />
+      <Route path="/sign-out" element={<Signout />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
